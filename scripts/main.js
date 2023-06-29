043afef82bd5c21b73ac9d37baec3c25e76400b3
Virga2942 <Virga3984@tutanota.com>
2023-06-29 04:59:35 -0400

    Implemented tile rendering

--- scripts/main.js
+++ scripts/main.js
@@ -2,37 +2,71 @@ const canvas = document.getElementById("viewport");
 const ctx = canvas.getContext("2d");
 
 let field = [
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1],
-	[2, 3, 4, 5, 0, 1, 2, 3, 4, 5],
-	[0, 1, 2, 3, 4, 5, 0, 1, 2, 3],
-	[4, 5, 0, 1, 2, 3, 4, 5, 0, 1]
+	[0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
+	[2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
+	[4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
+	[6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
+	[0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
+	[2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
+	[4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
+	[6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
+	[0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
+	[2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
+	[4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
+	[6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
+	[0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
+	[2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
+	[4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
+	[6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
+	[0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
+	[2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
+	[4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
+	[6, 7, 0, 1, 2, 3, 4, 5, 6, 7]
 ];
 
-img = new Image();
-img.src = "TetrisBlock.png"
-img.onload = function()
+let tetrisBlocks = [];
+let imgSources =
+[
+	"/images/GreyBlock.png",
+	"/images/RedBlock.png",
+	"/images/OrangeBlock.png",
+	"/images/YellowBlock.png",
+	"/images/GreenBlock.png",
+	"/images/CyanBlock.png",
+	"/images/BlueBlock.png",
+	"/images/PurpleBlock.png"
+];
+
+let loadCount = 0;
+
+for (let i = 0; i < imgSources.length; i++)
+{
+	let img = new Image();
+	img.src = imgSources[i];
+	tetrisBlocks.push(img);
+	tetrisBlocks[i].onload = function()
+	{
+		loadCount++;
+		if (loadCount == imgSources.length - 1)
+		{
+			startGame();
+		}
+	}
+}
+
+function startGame()
+{
+	renderScreen();
+}
+
+function renderScreen()
 {
 	for (let i = 0; i < 10; i++)
 	{
 		for (let j = 0; j < 20; j++)
 		{
-			ctx.drawImage(img, i * 25, j * 25, 25, 25);
+			//Render the block (with the color being determined by the field)
+			ctx.drawImage(tetrisBlocks[field[j][i]], i * 25, j * 25, 25, 25);
 			ctx.stroke();
 		}
 	}
