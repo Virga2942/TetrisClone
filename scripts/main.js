4379d6c7ccfaf0474621ef0518b3af22f9b9cf6e
Virga2942 <Virga3984@tutanota.com>
2023-07-03 21:57:51 -0400

    Added tetromino data.

--- scripts/main.js
+++ scripts/main.js
@@ -47,18 +47,241 @@ for (let i = 0; i < imgSources.length; i++)
 	tetrisBlocks[i].onload = function()
 	{
 		loadCount++;
-		if (loadCount == imgSources.length - 1)
+		if (loadCount == imgSources.length)
 		{
 			startGame();
 		}
 	}
 }
 
+class Tetromino
+{
+	constructor(colorIndex, uprightMatrix, rightMatrix, upsideDownMatrix, leftMatrix)
+	{
+		this.colorIndex = colorIndex;
+		this.matrices = [uprightMatrix, rightMatrix, upsideDownMatrix, leftMatrix];
+	}
+}
+
+//I don't really like calling them by these names but its easier for making the variables short so whatever ^\/(:/)\/^
+//Also this is pretty long so I might come back to this at some point and move it into another file or something, idk
+let zPiece = new Tetromino
+(
+	1,
+	//Upright
+	[
+		[1,1,0],
+		[0,1,1],
+		[0,0,0]
+	],
+	//Right
+	[
+		[0,0,1],
+		[0,1,1],
+		[0,1,0]
+	],
+	//Upside down
+	[
+		[0,0,0],
+		[1,1,0],
+		[0,1,1]
+	],
+	//Left
+	[
+		[0,0,1],
+		[0,1,1],
+		[0,1,0]
+	]
+);
+let lPiece = new Tetromino
+(
+	2,
+	//Upright
+	[
+		[0,0,1],
+		[1,1,1],
+		[0,0,0]
+	],
+	//Right
+	[
+		[0,1,0],
+		[0,1,0],
+		[0,1,1]
+	],
+	//Upside down
+	[
+		[0,0,0],
+		[1,1,1],
+		[1,0,0]
+	],
+	//Left
+	[
+		[1,1,0],
+		[0,1,0],
+		[0,1,0]
+	]
+);
+let oPiece = new Tetromino
+(
+	3,
+	//Upright
+	[
+		[0,1,1,0],
+		[0,1,1,0],
+		[0,0,0,0]
+	],
+	//Right
+	[
+		[0,1,1,0],
+		[0,1,1,0],
+		[0,0,0,0]
+	],
+	//Upside down
+	[
+		[0,1,1,0],
+		[0,1,1,0],
+		[0,0,0,0]
+	],
+	//Left
+	[
+		[0,1,1,0],
+		[0,1,1,0],
+		[0,0,0,0]
+	]
+);
+let sPiece = new Tetromino
+(
+	4,
+	//Upright
+	[
+		[0,1,1],
+		[1,1,0],
+		[0,0,0]
+	],
+	//Right
+	[
+		[0,0,1],
+		[0,1,1],
+		[0,1,0]
+	],
+	//Upside down
+	[
+		[0,1,1],
+		[1,1,0],
+		[0,0,0]
+	],
+	//Left
+	[
+		[0,0,1],
+		[0,1,1],
+		[0,1,0]
+	]
+);
+let iPiece = new Tetromino
+(
+	5,
+	//Upright
+	[
+		[0,0,0,0],
+		[1,1,1,1],
+		[0,0,0,0],
+		[0,0,0,0]
+	],
+	//Right
+	[
+		[0,0,1,0],
+		[0,0,1,0],
+		[0,0,1,0],
+		[0,0,1,0]
+	],
+	//Upside down
+	[
+		[0,0,0,0],
+		[0,0,0,0],
+		[1,1,1,1],
+		[0,0,0,0]
+	],
+	//Left
+	[
+		[0,1,0,0],
+		[0,1,0,0],
+		[0,1,0,0],
+		[0,1,0,0]
+	]
+);
+let rPiece = new Tetromino
+(
+	6,
+	//Upright
+	[
+		[1,0,0],
+		[1,1,1],
+		[0,0,0]
+	],
+	//Right
+	[
+		[0,1,1],
+		[0,1,0],
+		[0,1,0]
+	],
+	//Upside down
+	[
+		[0,0,0],
+		[1,1,1],
+		[0,0,1]
+	],
+	//Left
+	[
+		[1,1,0],
+		[0,1,0],
+		[0,1,0]
+	]
+);
+let tPiece = new Tetromino
+(
+	7,
+	//Upright
+	[
+		[0,1,0],
+		[1,1,1],
+		[0,0,0]
+	],
+	//Right
+	[
+		[0,1,0],
+		[0,1,1],
+		[0,1,0]
+	],
+	//Upside down
+	[
+		[0,0,0],
+		[1,1,1],
+		[0,1,0]
+	],
+	//Left
+	[
+		[0,1,0],
+		[1,1,0],
+		[0,1,0]
+	]
+);
+//Put all the tetrominos into an array sorted by hue for easy access
+let tetrominos = [zPiece, lPiece, oPiece, sPiece, iPiece, rPiece, tPiece];
+
+//Current piece (tuple? int array?)
+//[0] = current tetromino/piece
+//[1] = rotation
+//[2] = x position
+//[3] = y position
+let currentPiece = [0, 0, 0, 0];
+
+//Actual game code finally
 function startGame()
 {
 	renderScreen();
 }
 
+//Draw the board, update the UI elements
 function renderScreen()
 {
 	for (let i = 0; i < 10; i++)
@@ -71,3 +294,5 @@ function renderScreen()
 		}
 	}
 }
+
+//TODO: Make a function for rendering pieces based off of the current piece
